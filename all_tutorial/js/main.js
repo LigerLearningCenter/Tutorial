4dfe079aa5d33abd6232e0038122bd24257820ce
visalsao <visal.s@ligercambodia.org>
2016-05-25 09:34:54 +0700

    combine the 1 2 3 page together

--- all_tutorial/js/main.js
+++ all_tutorial/js/main.js
@@ -1,27 +1,39 @@
-//3rd Page code 
-	//Changing the point number 
-function convertToK(){
-	var moneyReal = document.getElementById("moneyInTheBank").innerHTML;
-	var moneyDisplay = "";
-	moneyReal = parseInt(moneyReal);
+$(document).ready(function() {
+	$( "#click-here" ).bind( "click", function ( event ) { 
+		$( "#page-2" ).show();
+		$( ".page-1" ).hide();
+		$( ".money" ).show();
+		$( "#page-2Next" ).show();
+	});
+	$( "#page-2Next" ).bind( "click", function ( event ) {
+		$( ".page-3" ).show();
+	})
+});
+
+// //3rd Page code 
+// 	//Changing the point number 
+// function convertToK(){
+// 	var moneyReal = document.getElementById("moneyInTheBank").innerHTML;
+// 	var moneyDisplay = "";
+// 	moneyReal = parseInt(moneyReal);
 	
-	if (moneyReal > 1000) {
-		var round = moneyReal/1000;
-		console.log(round);
-		moneyDisplay = (round.toString() + "K");
-		document.getElementById("moneyInTheBank").innerHTML = moneyDisplay;
-	}else{
-		document.getElementById("moneyInTheBank").innerHTML = moneyReal;
-	}
-}
-window.onload = convertToK;
+// 	if (moneyReal > 1000) {
+// 		var round = moneyReal/1000;
+// 		console.log(round);
+// 		moneyDisplay = (round.toString() + "K");
+// 		document.getElementById("moneyInTheBank").innerHTML = moneyDisplay;
+// 	}else{
+// 		document.getElementById("moneyInTheBank").innerHTML = moneyReal;
+// 	}
+// }
+// window.onload = convertToK;
 
-//6th Page code
-	//Get ting time 
-var time = new Date();
-var date = time.getDate();
-var month = time.getMonth();
-var year = time.getFullYear();
+// //6th Page code
+// 	//Get ting time 
+// var time = new Date();
+// var date = time.getDate();
+// var month = time.getMonth();
+// var year = time.getFullYear();
 
-var view = document.getElementById("timeZone").innerHTML = date + "." + month + "." + year;
-window.onload = convertToview;
+// var view = document.getElementById("timeZone").innerHTML = date + "." + month + "." + year;
+// window.onload = convertToview;
