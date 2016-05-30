03d1ecaadf6a0db0db653ea622804d1a757bb1b1
visalsao <visal.s@ligercambodia.org>
2016-05-30 15:13:39 +0700

    Update JS now at page 6
    Will try to finish page 7

--- all_tutorial/js/main.js
+++ all_tutorial/js/main.js
@@ -1,25 +1,47 @@
 $(document).ready(function() {
-    $( ".menubar" ).hide();
-//Will view Second
+    $(".menubar").hide();
+
+    //Will view Second
     $("#click-here").bind("click", function(event) {
         $("#page-2").show();
         $(".page-1").hide();
         $(".money").show();
         $("#page-2Next").show();
     });
-//Will view Third  
+    //Will view Third  
     $("#page-2Next").bind("click", function(event) {
         $(".page-3").show();
-        $( "page-3Next" ).show();
+        $("page-3Next").show();
         $("#page-2Next").hide();
     });
 
-//Will view Fourth
-    $( "#page-3Next" ).bind( "click", function(event){
-    	$( "#allArrow" ).show();
-    	$( ".page-3").hide();
-    	$( ".menubar" ).show();
-        $( "h1#heading").show();
+    //Will view Fourth
+    $("#page-3Next").bind("click", function(event) {
+        $("#allArrow").show();
+        $(".page-3").hide();
+        $("#page-4Next").show();
+        $(".menubar").show();
+        $("header").show();
+        $(".content").show();
+
+    });
+    //Will view Fifth
+    $("#page-4Next").bind("click", function(event) {
+        $("#allArrow").hide();
+        $(".down").show();
+        $("#page-4Next").hide();
+        $("#page-5Next").show();
+
+
+    });
+    //Will view sixth
+    $("#page-5Next").bind("click", function(event) {
+        $(".business").hide();
+        $(".show").show();
+        $(".down").hide();
+        $(".socialMood").show();
+        $( "#topBarsDateCollection" ).show();
+        $("#page-6Next").show();
     });
 });
 
@@ -40,25 +62,25 @@ $("#pause").click(function() {
 
 
 // //3rd Page code 
-// 	//Changing the point number 
+//  //Changing the point number 
 // function convertToK(){
-// 	var moneyReal = document.getElementById("moneyInTheBank").innerHTML;
-// 	var moneyDisplay = "";
-// 	moneyReal = parseInt(moneyReal);
+//  var moneyReal = document.getElementById("moneyInTheBank").innerHTML;
+//  var moneyDisplay = "";
+//  moneyReal = parseInt(moneyReal);
 
-// 	if (moneyReal > 1000) {
-// 		var round = moneyReal/1000;
-// 		console.log(round);
-// 		moneyDisplay = (round.toString() + "K");
-// 		document.getElementById("moneyInTheBank").innerHTML = moneyDisplay;
-// 	}else{
-// 		document.getElementById("moneyInTheBank").innerHTML = moneyReal;
-// 	}
+//  if (moneyReal > 1000) {
+//      var round = moneyReal/1000;
+//      console.log(round);
+//      moneyDisplay = (round.toString() + "K");
+//      document.getElementById("moneyInTheBank").innerHTML = moneyDisplay;
+//  }else{
+//      document.getElementById("moneyInTheBank").innerHTML = moneyReal;
+//  }
 // }
 // window.onload = convertToK;
 
 // //6th Page code
-// 	//Get ting time 
+//  //Get ting time 
 // var time = new Date();
 // var date = time.getDate();
 // var month = time.getMonth();
